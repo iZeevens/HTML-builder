2968df12d4310f1e6f07ada8d3a277ed0392aba6
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-20 18:22:42 +0600

    feat: add file size

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -12,9 +12,22 @@ fs.readdir(
     files.forEach((file) => {
       if (file.isFile()) {
         const fileName = file.name.toString();
-        console.log(
-          `${fileName.split('.')[0]}-${path.extname(fileName).slice(1)}`,
+        const filePath = path.join(
+          './03-files-in-folder/secret-folder',
+          fileName,
         );
+        fs.stat(filePath, (err, stat) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          console.log(
+            `${fileName.split('.')[0]}-${path
+              .extname(fileName)
+              .slice(1)}-${Math.ceil(stat.size / 1024)}kb`,
+          );
+        });
       }
     });
   },
