fa11f15e7cd5f5ec8f593467641fa742e55cdc26
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-21 22:49:03 +0600

    feat: add read and save the template file in a variable

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,10 +1,15 @@
 const fs = require('node:fs').promises;
-// const path = require('node:path');
+const path = require('node:path');
 
-const mainPath = './06-build-page/project-dist';
+// const mainPath = './06-build-page/project-dist';
+const pathBuilder = './06-build-page/';
 async function builderPage() {
   try {
-    
+    const dataTemplate = await fs.readFile(
+      path.join(pathBuilder, 'template.html'),
+      'utf-8',
+    );
+    dataTemplate.split('\n').forEach((item) => console.log(item));
     // await fs.mkdir(mainPath);
   } catch (err) {
     console.error(err);
