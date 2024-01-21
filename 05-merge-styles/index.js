12eaa8e70e6761f555bc762692845a913ca21ae8
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-21 21:42:29 +0600

    feat: add try/catch

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,26 +4,25 @@ const path = require('node:path');
 const pathStyles = './05-merge-styles/styles';
 const pathProject = './05-merge-styles/project-dist/Bundle.css';
 async function readCss() {
-  fs.readdir(pathStyles, (err, files) => {
-    let arrayStyles = [];
-    if (err) {
-      return console.error(err);
-    }
-    files.forEach((file) => {
-      if (path.extname(file).slice(1) === 'css') {
-        fs.readFile(path.join(pathStyles, file), 'utf8', (err, data) => {
-          if (err) {
-            return console.error(err);
-          }
+  try {
+    fs.readdir(pathStyles, (err, files) => {
+      let arrayStyles = [];
+
+      files.forEach((file) => {
+        if (path.extname(file).slice(1) === 'css') {
+          const filePath = path.join(pathStyles, file);
+          const data = await fs.readFile(filePath, 'utf8');
           arrayStyles.push(data);
-        });
-      }
+        }
+      }); 
+      fs.writeFile(pathProject, arrayStyles.join('\n'), (err) => {
+        if (err) {
+          return console.error(err);
+        }
+      });
     });
-    fs.writeFile(pathProject, arrayStyles.join('\n'), (err) => {
-      if (err) {
-        return console.error(err);
-      }
-    });
-  });
+  } catch{
+    console.error(err)
+  }
 }
 readCss();
