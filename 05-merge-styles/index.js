aae36a6a47068d667d8c7e43ea0545d71cdfa179
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-21 21:37:00 +0600

    feat: add async function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,24 +3,27 @@ const path = require('node:path');
 
 const pathStyles = './05-merge-styles/styles';
 const pathProject = './05-merge-styles/project-dist/Bundle.css';
-fs.readdir(pathStyles, (err, files) => {
-  let arrayStyles = [];
-  if (err) {
-    return console.error(err);
-  }
-  files.forEach((file) => {
-    if (path.extname(file).slice(1) === 'css') {
-      fs.readFile(path.join(pathStyles, file), 'utf8', (err, data) => {
-        if (err) {
-          return console.error(err);
-        }
-        arrayStyles.push(data);
-      });
-    }
-  });
-  fs.writeFile(pathProject, arrayStyles.join('\n'), (err) => {
+async function readCss() {
+  fs.readdir(pathStyles, (err, files) => {
+    let arrayStyles = [];
     if (err) {
       return console.error(err);
     }
+    files.forEach((file) => {
+      if (path.extname(file).slice(1) === 'css') {
+        fs.readFile(path.join(pathStyles, file), 'utf8', (err, data) => {
+          if (err) {
+            return console.error(err);
+          }
+          arrayStyles.push(data);
+        });
+      }
+    });
+    fs.writeFile(pathProject, arrayStyles.join('\n'), (err) => {
+      if (err) {
+        return console.error(err);
+      }
+    });
   });
-});
+}
+readCss();
