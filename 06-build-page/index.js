939412f049afc267405ff67c74e67aced3fb1917
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-22 23:47:16 +0600

    feat: add replacePlaceholder

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,18 +1,35 @@
 const fs = require('node:fs').promises;
 const path = require('node:path');
 
-// const mainPath = './06-build-page/project-dist';
 const pathBuilder = './06-build-page/';
+
 async function builderPage() {
   try {
-    const dataTemplate = await fs.readFile(
+    let dataTemplate = await fs.readFile(
       path.join(pathBuilder, 'template.html'),
       'utf-8',
     );
-    dataTemplate.split('\n').forEach((item) => console.log(item));
-    // await fs.mkdir(mainPath);
+
+    const componentsPath = path.join(pathBuilder, 'components');
+
+    const replacePlaceholder = async (placeholder) => {
+      const placeholderRegex = new RegExp(`{{${placeholder}}}`, 'g');
+      const placeholderFile = path.join(componentsPath, `${placeholder}.html`);
+
+      try {
+        const fileContent = await fs.readFile(placeholderFile, 'utf-8');
+        dataTemplate = dataTemplate.replace(placeholderRegex, fileContent);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    await Promise.all(['header', 'articles', 'footer'].map(replacePlaceholder));
+
+    console.log(dataTemplate);
   } catch (err) {
     console.error(err);
   }
 }
+
 builderPage();
