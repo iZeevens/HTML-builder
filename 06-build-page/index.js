f46e27a3aca93d776136966fdb9e80c2143595c8
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-23 00:00:07 +0600

    feat: add readCss

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -26,7 +26,36 @@ async function builderPage() {
 
     await Promise.all(['header', 'articles', 'footer'].map(replacePlaceholder));
 
-    console.log(dataTemplate);
+    await fs.mkdir(path.join(pathBuilder, 'project-dist'), { recursive: true });
+    await fs.writeFile(
+      path.join(pathBuilder, 'project-dist/index.html'),
+      dataTemplate,
+    );
+
+    const readCss = async (pathStyles, pathProject) => {
+      try {
+        const files = await fs.readdir(pathStyles);
+        const arrayStyles = [];
+
+        for (const file of files) {
+          if (path.extname(file).slice(1) === 'css') {
+            const data = await fs.readFile(
+              path.join(pathStyles, file),
+              'utf-8',
+            );
+            arrayStyles.push(data);
+          }
+        }
+
+        await fs.writeFile(pathProject, arrayStyles.join('\n'), 'utf-8');
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    readCss(
+      path.join(pathBuilder, 'styles'),
+      path.join(pathBuilder, 'project-dist/style.css'),
+    );
   } catch (err) {
     console.error(err);
   }
